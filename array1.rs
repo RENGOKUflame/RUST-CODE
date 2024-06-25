c2c977d2d5aeb9cf897f1531647530acc632649a
RENGOKUflame <140197110+RENGOKUflame@users.noreply.github.com>
2024-06-25 06:50:07 -0700

    Create array1.rs

---
+++ array1.rs
@@ -0,0 +1,10 @@
+// printing an array....
+    fn main(){ 
+        let arr = [1,2,3,4,5];
+        println!("The entire array is:{:?}",arr);
+        for number in arr{
+            println!("Each element of array is:{}",number);
+            }
+            
+        }
+    
